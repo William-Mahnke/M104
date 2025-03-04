d2912f584db138e2d24091385d0683e2f5d020ba
William-Mahnke <146669951+William-Mahnke@users.noreply.github.com>
2025-03-04 13:20:15 -0800

    Update Bisection.py
    flushed out function to be complete 
    WIP - add stopping measure with associated epsilon value

--- Bisection.py
+++ Bisection.py
@@ -1,13 +1,22 @@
+# import numpy to aid with creating functions
 import numpy as np
 
-def bisection(a,b,f, max_int = 500):
+def bisection(a, b, f, max_int = 500):
+    '''
+    Uses the bisection method to find a root for a function in a defined interval
+    Inputs:
+    a, b - interval (note a < b)
+    f - function interested in finding a root for
+    max_int - maximum number of iterations to perform
+    Output:
+    c - approximate root 
+    '''
+    # optional addition - add tolerance level so method stops if new value c
+    # is relatively close to previous values (for some defined epsilon)
     for i in range(max_int):
-        c = (a+b)/2
-        if f(a)*f(c) <= 0:
+        c = (a + b) / 2
+        if f(a) * f(c) <= 0:
             b = c
         else:
             a = c
     return c
-
-f = lambda x: np.log10(x-9)
-print(bisection(2,15,f))
